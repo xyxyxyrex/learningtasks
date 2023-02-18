036e86c4223fff8442c19c729873c4cfbdaad648
xyxyxyrex <122286060+xyxyxyrex@users.noreply.github.com>
2023-02-18 23:36:55 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+!DOCTYPE html>
+<html>
+  <head>
+        <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> Welcome to my Website!! I am Xyrex!</h1>
+  <ul>
+      <li> <a href ="index.php"> HOME </a></li>
+      <li><a href=" /LT2/index.thml"> LEARNING TASK 2</a> </li>
+      </ul>
+      </body>
+      </html>
